1320abc19cb2d035fe840d7ca6c1aa8b6dd4abea
kfradd <ken@fradd.co.uk>
2021-05-12 21:01:40 +0100

    Create folder named views, then create loginForm, registerForm HTML pages int that folder

---
+++ src/main/java/com/example/postgresqldemo/controller/memberController.java
@@ -0,0 +1,20 @@
+package com.example.postgresqldemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class memberController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "views/loginForm";
+    }
+
+    @GetMapping("/register")
+    public String registerForm(Model model) {
+        return "views/registerForm";
+    }
+
+}
